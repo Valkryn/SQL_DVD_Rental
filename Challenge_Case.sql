db483cfd1c37ca77b02f183edd40c2e69daa1271
Calvin Gutierrez <61283082+Valkryn@users.noreply.github.com>
2022-09-09 22:57:37 -0400

    Update Challenge_Case.sql
    renamed title to specifiy sql file and commented out the questions / solutions

--- Challenge_Case.sql
+++ Challenge_Case.sql
@@ -1,19 +1,19 @@
-Challenge -
-Using the customer list, make the first 100 customers a status of Gold , customer 100 -> 200 Silver and the rest Bronze
+-- Challenge -
+-- Using the customer list, make the first 100 customers a status of Gold , customer 100 -> 200 Silver and the rest Bronze
 
-Solution -
+-- Solution -
 SELECT customer_id ,
 	CASE
 		WHEN customer_id <= 100 THEN 'Gold'
-		WHEN customer_id < 200 THEN 'Silver' // this line can also say WHEN customer_id BETWEEN 100 AND 200 THEN 'Silver
+		WHEN customer_id < 200 THEN 'Silver' // this line can also say WHEN customer_id BETWEEN 100 AND 200 THEN 'Silver'
 		ELSE 'Normal'
 	END AS customer_status
 FROM customer;
 
-Challenge - 
-There was a raffle. Using the customer list write a query returning customer with id 2 as the winner and customer 5 as second place
+-- Challenge - 
+-- There was a raffle. Using the customer list write a query returning customer with id 2 as the winner and customer 5 as second place
 
-Solution -
+-- Solution -
 
 SELECT customer_id,
 	CASE customer_id
@@ -23,10 +23,10 @@ SELECT customer_id,
 	END AS raffle_winner
 FROM customer;
 
-Challenge -
-We want to know and compare the various amounts of films we have  per movie rating. Use CASE and the dvdrental database to re-create this table. 
+-- Challenge -
+-- We want to know and compare the various amounts of films we have  per movie rating. Use CASE and the dvdrental database to re-create this table. 
 
-solution-
+-- solution-
 SELECT 
 	SUM(CASE rating
 		WHEN 'R' THEN 1
