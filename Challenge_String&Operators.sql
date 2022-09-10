9a435c3b3bcf479a1f2a7f3c1ccf40e4999514f2
Calvin Gutierrez <61283082+Valkryn@users.noreply.github.com>
2022-09-09 22:59:25 -0400

    Update Challenge_String&Operators.sql
    renamed title to specifiy sql file and commented out the questions / solutions

--- Challenge_String&Operators.sql
+++ Challenge_String&Operators.sql
@@ -1,22 +1,22 @@
-Challenge 1 -
-Combine first and last name under a single column
+-- Challenge 1 -
+-- Combine first and last name under a single column
 
-Solution
+-- Solution
 SELECT first_name || ' ' || last_name AS full_name FROM customer;
 
-Challenge 2 -
-Create an email address for each person following the format first.last@sql.com
+-- Challenge 2 -
+-- Create an email address for each person following the format first.last@sql.com
 
-Solution -
+-- Solution -
 SELECT first_name || '.' || last_name ||'@sql.com' AS full_name FROM customer;
 
-NOTE: || combines/concatenates strings together
+-- NOTE: || combines/concatenates strings together
 
-Challenge 3 -
-Create an email address for each person following the format using the first 3 letters of first name '.' lastname. 
-Ensure the whole address is lower case.
+-- Challenge 3 -
+-- Create an email address for each person following the format using the first 3 letters of first name '.' lastname. 
+-- Ensure the whole address is lower case.
 
-Solution - 
+-- Solution - 
 SELECT LOWER(LEFT(first_name,3)) || '.' || LOWER(last_name) ||  '@sql.com' AS email
 	FROM customer;
 
