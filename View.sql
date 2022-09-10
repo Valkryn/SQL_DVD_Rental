630e44981c8a1d67918c30e543904acef1e877ff
Calvin Gutierrez <61283082+Valkryn@users.noreply.github.com>
2022-09-09 23:01:13 -0400

    Update View.sql
    renamed title to specifiy sql file and commented out the questions / solutions

--- View.sql
+++ View.sql
@@ -1,6 +1,6 @@
-A view is a bit of a shortcut for a common query that one will find themself performing. 
-for exmaple, if i find myself constantly joining 2 tables such as customer and address to know the name and address (info) of a customer;  
-then making a VIEW will be a good option instead of re-writing the query over and over
+-- A view is a bit of a shortcut for a common query that one will find themself performing. 
+-- for exmaple, if i find myself constantly joining 2 tables such as customer and address to know the name and address (info) of a customer;  
+-- then making a VIEW will be a good option instead of re-writing the query over and over
 
 
 CREATE VIEW customer_info AS
@@ -8,24 +8,24 @@ SELECT first_name || ' ' || last_name AS full_name , email , address FROM custom
 	INNER JOIN address
 		ON customer.address_id = address.address_id;	
     
-This will create a view called 'customer_info'
+-- This will create a view called 'customer_info'
 
-and to call this VIEW 
+-- and to call this VIEW 
 SELECT * FROM customer_info;
 
-To alter a VIEW use
-CREATE OR REPLACE VIEW /VIEW_NAME/ AS 
-query.. .. .. 
+-- To alter a VIEW use
+-- CREATE OR REPLACE VIEW /VIEW_NAME/ AS 
+-- query.. .. .. 
 
 
-CREATE OR REPLACE VIEW customer_info AS 
+-- CREATE OR REPLACE VIEW customer_info AS 
 SELECT first_name || ' ' || last_name AS full_name , email , address , district FROM customer
 	INNER JOIN address
 		ON customer.address_id = address.address_id;
     
- To remove a view 
+--  To remove a view 
  DROP VIEW customer_info
  
- To rename a view 
+--  To rename a view 
  ALTER VIEW customer_info RENAME TO c_info 
  
